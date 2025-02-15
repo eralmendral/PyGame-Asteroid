0638bd9c9406f6b082a914e47efff3992a45f5cf
Eric Almendral <eralmndrx@gmail.com>
2025-02-15 23:03:06 +0800

    fps

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
+	Clock = pygame.time.Clock()
+	dt = 0
+
+
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 					return
+		dt = Clock.tick(60) / 1000
 		screen.fill((0, 0, 0))
 		pygame.display.flip()
 	
